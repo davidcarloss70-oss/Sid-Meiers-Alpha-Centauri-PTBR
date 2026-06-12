6d58e2485557ae8d269f127e210eba18ef675eac
davidcarloss70-oss <davidcarloss70@gmail.com>
2026-06-12 02:45:16 -0300

    Clean up root temporary utility scripts

--- check.py
+++
@@ -1,4 +0,0 @@
-import json
-with open('Tools/translation_cache.json', encoding='utf-8') as f:
-    data = json.load(f)
-print(data['As to myself, I remain $TITLE2 $NAME3 of the $FACTION4. My intention'])
